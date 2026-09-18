3ce7a946c9bbaf4621428dbdaf4984fe22b2521d
kurut839-sys <kurut839@gmail.com>
2026-09-18 15:59:22 +0600

    first commit

---
+++ lesson1/index.js
@@ -0,0 +1,14 @@
+// alert("helo world");
+
+// confirm("helo world");
+
+// promt( "what is your name?");
+
+// console.log("helo world")
+
+// let name ="alisher"
+
+// alert(name)
+
+let name= prompt("what is your name ")
+alert(name)
