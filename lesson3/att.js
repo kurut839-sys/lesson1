cdf6720ac60a607223e7b499a3c69164a30c8081
kurut839-sys <kurut839@gmail.com>
2026-09-22 16:07:14 +0600

    first commit

---
+++ lesson3/att.js
@@ -0,0 +1,27 @@
+//  условия оператора if swich/case
+
+// let email = promt ('ведите ваш email');
+//  let pasword = promt ('ведите ваш возраст');
+// let userEmail = 'админ'
+// let userPasswod = 'admin11'
+// if (email === userEmail && password === userpassword){
+//    alert( "вы успешно вошли вы систему" )
+// }else{
+    // alert ("  не верный логин или пароль")
+// }
+
+let month = prompt('ведите месяц')
+switch(month){
+    case ' октябр ':
+    case 'декабр':
+    alert('кыш');
+    break;
+    
+    case ' октябр ':
+    case 'декабр':
+    alert('кыш')
+    break;
+
+    default:
+    alert(' вы ощиблись месяцом');
+}
